78dbb5a5120bc7671d4086585686f4d34aa46891
girlfind <42343458+girlfind@users.noreply.github.com>
2019-10-27 13:09:22 +0200

    Add files via upload

---
+++ Mad Lİbs Game.py
@@ -0,0 +1,16 @@
+
+adjective = input("Enter a adjective: ")
+print("Once that "+ adjective + " music comes on, it's time to shut down your acceptance speech!")
+
+Body_Part = input("Enter a Body Part: ")
+print("Basketball is the best " + Body_Part + " in the world")
+
+NOUN = input("Enter a Noun: ")
+print("Be careful not to vacuum the " +  NOUN + " when you clean under your bed.")
+
+NOUN = input("Enter a Noun: ")
+print("Love is what makes the " + NOUN + " go round.")
+
+VERB_ENDING_IN_ING = input("Enter a Verb ending ın -ing: ")
+print("My daily exercise is " + VERB_ENDING_IN_ING + " after my school bus in the morning!")
+
